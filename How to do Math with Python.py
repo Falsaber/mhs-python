67b5ea3e71072e37571f816e24fb40321963a264
Isaiah Hersh <isaiahhersh@hotmail.com>
2016-12-12 10:44:34 -0500

    Simple math operations

---
+++ How to do Math with Python.py
@@ -0,0 +1,25 @@
+#Simple math equations can easily be solved using Python. You have access to a good amount of symbols to use for this purpose (+,-,*,/,%).
+
+#Addition
+add = 2 + 2
+print(add)
+
+#Subtraction
+subtract = 95 - 65
+print(subtract)
+
+#Multiplication
+times = 5 * 6
+print(times)
+
+#Division
+divide = 88 / 2
+print(divide)
+
+#Exponentials
+power = 9 ** 3
+print(power)
+
+#Modulo (Remainder)
+remain = 3 % 2
+print(remain)
