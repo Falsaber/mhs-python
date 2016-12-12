efecf026a7658523daf9e12e9d5672f1a4f9bb98
Isaiah Hersh <isaiahhersh@hotmail.com>
2016-12-12 10:50:23 -0500

    Added tip calculator example section

--- How to do Math with Python.py
+++ How to do Math with Python.py
@@ -23,3 +23,13 @@ print(power)
 #Modulo (Remainder)
 remain = 3 % 2
 print(remain)
+
+#Once variables are defined, they can also be used to perform more complex equations, like calculating the tip for a meal:
+meal = 21.50
+tax = 0.05
+tip = 0.2
+#Note that variables can actually be redefined by their previous definitions! This allows us to set a new value for "meal" and after, "total":
+meal = meal + meal * tax
+total = meal + meal * tip
+#And then, we can find the total, using the modulo function.
+print("%.2f" % total)
